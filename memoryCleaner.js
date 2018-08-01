7a5dece6a52c65ff972fc00aeab88b4ac6918e63
Pascal Schaffner <pascal.schaffner@hotmail.com>
2018-08-01 14:48:02 +0200

    refactoring

--- memoryCleaner.js
+++ memoryCleaner.js
@@ -1,13 +1,18 @@
-// memory cleaning
-module.exports = {
-    
-        // löscht alle nicht existierende Creeps
-        run: function(){
-        for(var name in Memory.creeps) {
-            if(!Game.creeps[name]) {
-                delete Memory.creeps[name];
-            }
+/*
+ *  memory cleaning
+ */ 
+ 
+// löscht alle nicht existierende Creeps
+function CleanOldCreeps(){
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
         }
     }
-
+}
+ 
+module.exports = {
+    run: function(){
+        CleanOldCreeps();
+    }
 };
